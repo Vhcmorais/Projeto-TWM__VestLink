5fa1d522d970a96607ca5676720739cfe7365c05
Vitor Henrique <vhcmoraiss@yahoo.com>
2025-09-05 10:40:23 -0300

    Update DB

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,5 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
